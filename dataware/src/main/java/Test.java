bb163a3659800c38168cd9a69ac2c8327b521a1b
樊志伟 <1063181569@qq.com>
2020-12-20 13:25:59 +0800

    测试上传代码案例Hello world

---
+++ dataware/src/main/java/Test.java
@@ -0,0 +1,12 @@
+import io.netty.buffer.PoolSubpageMetric;
+import sun.applet.Main;
+
+/**
+ * @author charley
+ * 2020-12-20-20
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
